6be5979752274d8a6bc228d79a30aabf53d88926
Benjamin Erhart <berhart@netzarchitekten.com>
2025-10-06 14:12:48 +0200

    Added investigated links.

--- IdentityProviderExtension/DocumentProviderExtension.swift
+++ IdentityProviderExtension/DocumentProviderExtension.swift
@@ -10,6 +10,25 @@ import IdentityDocumentServicesUI
 import IdentityDocumentServices
 import SwiftUI
 
+/**
+ Only works with Andoird using OpneID4VP:
+  https://digital-credentials.dev
+ https://www.corbado.com/blog/how-to-build-verifiable-credential-issuer
+
+ https://developer.mozilla.org/en-US/docs/Web/API/CredentialsContainer/get
+
+ https://developer.apple.com/videos/play/wwdc2025/232/
+ https://developer.apple.com/documentation/IdentityDocumentServices/Implenting-as-an-identity-document-provider
+ https://developer.apple.com/wallet/get-started-with-verify-with-wallet/
+
+ HPKE: https://datatracker.ietf.org/doc/rfc9180/
+ ISO 18013-7:
+ - https://www.iso.org/obp/ui#iso:std:iso-iec:ts:18013:-7:ed-2:v1:en
+ - https://cdn.standards.iteh.ai/samples/91154/49f64e2fad774d6d8da6c1b326c207e6/ISO-IEC-DTS-18013-7.pdf?utm_source=chatgpt.com
+
+ https://oneiam.medium.com/understanding-iso-18013-5-and-iso-18013-7-the-standards-shaping-mobile-drivers-licenses-mdls-88a4287ac367
+
+ */
 @main
 struct DocumentProviderExtension: IdentityDocumentProvider {
 
